a984ed9485b8499cf03985314806457a5572fd6b
MwebeCoder27 <mwebraymond13@gmail.com>
2024-08-23 16:41:55 +0300

    add loadSong function to pinia store

--- spotifyClone/src/stores/song.js
+++ spotifyClone/src/stores/song.js
@@ -2,7 +2,32 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useSongStore = defineStore('song', () => {
-  const count = ref(0)
+  const isPlaying = ref(false)
+  const audio = ref(null)
+  const currentArtist = ref(null)
+  const currentTrack = ref(null)
+
+  //actions
+  const loadSong = (artist, track) => {
+    currentArtist.value = artist
+    currentTrack.value = track
+
+    
+    if (audio.value && audio.src.value){
+      audio.value.pause()
+      isPlaying.value = false
+      audio.src.value = ''
+    }
+
+    audio.value = new Audio()
+    audio.src.value = track.path
+
+    setTimeout(() => {
+      isPlaying.value = false
+      audio.value.play()
+    }, 200);
+  }
+
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
