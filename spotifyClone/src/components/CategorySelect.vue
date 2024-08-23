19ee9343df740272b012625fb7709081c419a9b7
MwebeCoder27 <mwebraymond13@gmail.com>
2024-08-23 12:37:55 +0300

    fixes style bugs

---
+++ spotifyClone/src/components/CategorySelect.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { toRefs, ref } from "vue";
+import { RouterLink } from "vue-router";
+import uniqolor from "uniqolor";
+let randColor = ref("");
+
+const props = defineProps({ category: String, image: String });
+const { category } = toRefs(props);
+</script>
+
+<template>
+  <RouterLink to="library">
+    <div
+      :style="`background-color:${randColor.color}`"
+      class="contrast-[2] brightness-[0.87] rounded-xl aspect-square relative overflow-hidden"
+    >   
+    <div class="text-white pt-3 pl-4 font-semibold text-[23px]">
+        {{category}}
+    </div>
+    <img :src="image || ''" alt="" 
+    class="absolute bottom-0 -right-3.5 max-w-[100px] contrast-[0.55] rotate-[20deg]">
+</div>
+  </RouterLink>
+</template>
---
+++ spotifyClone/src/views/SearchView.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import CategorySelect from '@/components/CategorySelect.vue';
+</script>
+
+<template>
+    <div class="p-8">
+    <button
+    type="button"
+    class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+      Podcasts to make you smarter
+    </button>
+
+    <div class="py-1.5"></div>
+
+    <div class="flex items-center">
+      <CategorySelect image="https://picsum.photos/id/40/300/300" category="Podcasts" title="Title is here" subTitle="Subtitle is here" />
+
+    </div>
+  </div>
+</template>
